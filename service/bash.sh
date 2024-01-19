01c6902dbf67f4b62fcb0a1cca9a13c5391f1f97
aaron woo <707230686@qq.com>
2024-01-19 17:30:07 +0800

    update bash.sh

--- service/bash.sh
+++ service/bash.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
-
-/usr/local/bin/php /sites/service/command/migrate.php
-/usr/local/bin/php /sites/service/index.php
+bashdir=`cd $(dirname $0); pwd -P`
+/usr/bin/composer install --working-dir=$bashdir
+/usr/local/bin/php $bashdir/command/migrate.php
+/usr/local/bin/php $bashdir/index.php
