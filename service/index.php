8ed69c82f5fa77a996063e7dc4179f5206928b8b
aaronwoo <707230686@qq.com>
2024-01-20 09:52:03 +0800

    remove useless code

--- service/index.php
+++ service/index.php
@@ -3,14 +3,10 @@ require_once __DIR__.'/vendor/autoload.php';
 
 use Swoole\Http\Request;
 use Swoole\Http\Response;
-use app\controller\TestController;
-use Elastic\Elasticsearch\ClientBuilder;
 use app\core\App;
 
-$client = ClientBuilder::create()->setHosts(['es:9200'])->build();
-
 $http = new Swoole\Http\Server("0.0.0.0", 80);
-$http->on('request', function (Request $request, Response $response) use ($client) {
+$http->on('request', function (Request $request, Response $response) {
     App::handle($request, $response);
 });
 $http->start();
